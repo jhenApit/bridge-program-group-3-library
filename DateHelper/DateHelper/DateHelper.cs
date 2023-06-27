1c27dee3643dcda463ee136b9116976dd16d5c6c
arvinarar <66163872+arvinarar@users.noreply.github.com>
2023-06-27 14:36:38 +0800

    Add CalculateAge method

--- DateHelper/DateHelper/DateHelper.cs
+++ DateHelper/DateHelper/DateHelper.cs
@@ -14,6 +14,19 @@ namespace DateHelperLibrary
             return date.DayOfWeek;
         }
 
+        public int CalculateAge(DateTime birthdate)
+        {
+            int age = DateTime.Now.Year - birthdate.Year;
+
+            if (DateTime.Now.Month < birthdate.Month ||
+                (DateTime.Now.Month == birthdate.Month && DateTime.Now.Day < birthdate.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
         public string FormatDateTime(DateTime dateTime, string format)
         {
             return dateTime.ToString(format);
