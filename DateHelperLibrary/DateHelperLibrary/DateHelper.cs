28cf1773aebab48d9e5df530f7e57f0ccb2df218
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-06-27 16:44:26 +0800

    Rename Class

--- DateHelperLibrary/DateHelperLibrary/DateHelper.cs
+++ DateHelperLibrary/DateHelperLibrary/DateHelper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DateHelperLibrary
 {
-    public class DateHelperClass
+    public class DateHelper
     {
         /// <summary>
         /// Gets the current date time.
